efe8bff39b99d39b10b4592077f876f040bf2a1c
michael Ortiz <michaelortiz@michaels-MacBook-Pro-2.local>
2017-07-22 19:08:45 -0400

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,32 +27,53 @@ function show(){
                 for (var i = 0; i < res.length; i++){
                     productsArray.push(res[i].itemname);
                 }
-                start();
                 return productsArray;
             },
-        });
+            message:"Please input the item ID of the item you wish to purchase"
+        }).then(function(answer){
+            for(var i = 0; i < res.length; i++){
+                if(res[i].itemname === answer.choice){
+                    var chosenItem = res[i];
+                    inquirer.prompt({
+                        name:"quantity",
+                        type:"input",
+                        message:"how many units of this product do you want?"
+                    }).then(function(answer){
+                        connection.query(
+                            "UPDATE products SET? WHERE ?",[{
+                                stock_quantity: stock_quantity - answer.quantity
+                            }], function(err,res){
+                                console.log("item successfully purchased");
+                                console.log("New quantity is now: " + stock_quantity);
+                            }
+                            
+                        )
+                    })
+                }
+            }
+        })
+
     });
 };
 
 show();
-
+/*
 function start(){
     inquirer.prompt({
         name:"askForId",
         type:"input",
-        message:"Please input the product ID of the item you wish to purchase",
+        message:"",
     },{
         name:"quantity",
         type:"input",
         message:"how many units of this product do you want?"
-    }).then(function(answer){
+    });/*.then(function(answer){
         if (answer!){
         connection.query(
         "UPDATE products SET ? WHERE ?",
-        /*[{
+        [{
             products.stock_quantity: products.stock_quantity - answer.quantity
-        }]*/
+        }]
     
     )}
-    });
-};
+    });*/
